ce8591ec13c232c6b6496f910ee5344c228c1126
Tom Aldcroft <taldcroft@gmail.com>
2015-11-14 12:43:36 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cxotime',
+      author='Tom Aldcroft',
+      description='Chandra Time class base on astropy Time',
+      author_email='taldcroft@cfa.harvard.edu',
+      version='0.1',
+      zip_safe=False,
+      packages=['cxotime'],
+      )
