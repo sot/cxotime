52f48941e92484aac24dd7fcedc16435190f675c
Tom Aldcroft <taldcroft@gmail.com>
2015-11-16 08:52:44 -0500

    Update tests

--- test.py
+++ test.py
@@ -25,12 +25,47 @@ def test_cxotime_basic():
     # Multi-dim input
     t = CxoTime([[1, 2], [3, 4]])
     assert t.shape == (2, 2)
-    assert np.all(t.tt.date == [['1998:001:00:00:01.000', '1998:001:00:00:02.000'],
-                                ['1998:001:00:00:03.000', '1998:001:00:00:04.000']])
+    t_date = [['1998:001:00:00:01.000', '1998:001:00:00:02.000'],
+              ['1998:001:00:00:03.000', '1998:001:00:00:04.000']]
+    assert np.all(t.tt.date == t_date)
 
     t = CxoTime('1998:001:00:00:01.000', scale='tt')
     assert t.scale == 'tt'
     assert np.allclose(t.secs, 1.0, atol=1e-10, rtol=0)
+
+
+@pytest.mark.xfail
+def test_secs():
+    """
+    Test a problem fixed in https://github.com/astropy/astropy/pull/4312.
+    This test would pass for ``t = CxoTime(1, scale='tt')`` or if
+    comparing t.secs to 1.0.
+    """
+    t = CxoTime(1)  # scale = UTC
+    assert np.allclose(t.value, 1.0, atol=1e-10, rtol=0)
+
+
+def test_date():
+    t = CxoTime('2001:002:03:04:05.678')
+    assert t.format == 'date'
+    assert t.scale == 'utc'
+
+
+def test_greta():
+    """Test greta format"""
+    t_in = [['2001002.030405678', '2002002.030405678'],
+            ['2003002.030405678', '2004002.030405678']]
+    t = CxoTime(t_in)
+    assert t.format == 'greta'
+    assert t.scale == 'utc'
+    assert t.shape == (2, 2)
+    assert np.all(t.yday == [['2001:002:03:04:05.678', '2002:002:03:04:05.678'],
+                             ['2003:002:03:04:05.678', '2004:002:03:04:05.678']])
+    assert np.all(t.value == t_in)
+
+    # Input from float
+    t = CxoTime(np.array(t_in, dtype=np.float), format='greta')
+    assert np.all(t.value == t_in)
 
 
 @pytest.mark.skipif('not HAS_DATETIME')
