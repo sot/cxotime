f22dc90da0b36b24267611a69321fba64c051763
Tom Aldcroft <taldcroft@gmail.com>
2017-01-02 19:41:12 -0500

    Modernize tests

--- cxotime/__init__.py
+++ cxotime/__init__.py
@@ -1,3 +1,11 @@
 from .cxotime import CxoTime
 
 __version__ = '0.1'
+
+
+def test(*args, **kwargs):
+    '''
+    Run py.test unit tests.
+    '''
+    import testr
+    return testr.test(*args, **kwargs)
--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup
 
 from cxotime import __version__
 
+try:
+    from testr.setup_helper import cmdclass
+except ImportError:
+    cmdclass = {}
+
 setup(name='cxotime',
       author='Tom Aldcroft',
       description='Chandra Time class base on astropy Time',
       author_email='taldcroft@cfa.harvard.edu',
       version=__version__,
-      packages=['cxotime'],
+      zip_safe=False,
+      packages=['cxotime', 'cxotime.tests'],
+      tests_require=['pytest'],
+      cmdclass=cmdclass,
       )
