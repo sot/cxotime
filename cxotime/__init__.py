6bbf3e34ae0bbd257d4808a18b7d79e0c133a6f9
Tom Aldcroft <taldcroft@gmail.com>
2020-09-27 06:13:09 -0400

    Add flake8 checking workflow

--- cxotime/__init__.py
+++ cxotime/__init__.py
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from .cxotime import CxoTime
+from .cxotime import CxoTime  # noqa
 import ska_helpers
 
 __version__ = ska_helpers.get_version(__package__)
